6a02e63e557de95b194d6505fd69bb8e22626a00
Ze Jin <zj58@en-cs-totient-01.coecis.cornell.edu>
2015-10-29 18:58:19 -0400

    update explicit offload

---
+++ 4/strong.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+ncell=200
+
+for nthread in {1..8}
+do
+file="strong_${nthread}.pbs"
+
+echo "#!/bin/sh -l" >> $file
+echo "#PBS -l nodes=1:ppn=24" >> $file
+echo "#PBS -l walltime=0:30:00" >> $file
+echo "#PBS -N shallow" >> $file
+echo "#PBS -j oe" >> $file
+echo "module load cs5220" >> $file
+echo "cd \$PBS_O_WORKDIR" >> $file
+
+echo "echo 'strong scaling'">> $file
+echo "echo 'number of threads: $((nthread*nthread))'" >> $file
+echo "echo 'number of cells: $ncell'" >> $file
+
+#echo "export OMP_NUM_THREADS=$nthreads" >> $file
+echo "./lshallow tests.lua dam $ncell $((nthread*nthread)) 10 strong" >> $file
+
+qsub $file
+rm $file
+done
